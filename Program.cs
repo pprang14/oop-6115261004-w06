7ca93c2ab5d15719b8321cd6cd66f3377e226735
pprang14 <61750419+pprang14@users.noreply.github.com>
2020-08-23 19:35:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oop_6115261004_w06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person("Chirapat", "", 26000, "Lecturer");
+            Person per2 = new Person("wannakarn", "", 16000, "Staff");
+            Person per3 = new Person("katiya", "", 15000, "Staff");
+            Faculty f1 = new Faculty("Faculty of Educational");
+            f1.setPerson(per1, 0);
+            f1.setPerson(per2, 1);
+            f1.setPerson(per3, 2);
+            f1.printAll();
+
+        }
+    }
+}
